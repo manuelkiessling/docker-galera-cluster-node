a3cb7149dd234f591c27f0c497193f58cddb6862
Manuel Kiessling <manuel@kiessling.net>
2014-10-22 18:38:45 +0200

    Started working on confd integration

--- files/opt/start.sh
+++ files/opt/start.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-echo $ETCD_ENDPOINT
-echo $GALERA_WSREP_NODE_ADDRESS
 echo $GALERA_WSREP_NODE_ADDRESS >> /etc/mysql/conf.d/wsrep_node_address.cnf
 
-while true; do echo "hello"; sleep 2; done
-#service mysql start
+confd -interval 10 -node $ETCD_ENDPOINT -config-file /etc/confd/conf.d/mysql-galera.toml &
 
-# Start confd watcher script
+service mysql start
+
+while true; sleep 2; done
