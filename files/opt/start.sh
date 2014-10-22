5cc5aeca347892d017b362ab259da343fbebb42f
Manuel Kiessling <manuel@kiessling.net>
2014-10-22 16:17:34 +0000

    Passing and setting Galera wsrep_node_address value works

--- files/opt/start.sh
+++ files/opt/start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+echo $ETCD_ENDPOINT
+echo $GALERA_WSREP_NODE_ADDRESS
 echo $GALERA_WSREP_NODE_ADDRESS >> /etc/mysql/conf.d/wsrep_node_address.cnf
+
 while true; do echo "hello"; sleep 2; done
-service mysql start
+#service mysql start
 
 # Start confd watcher script
