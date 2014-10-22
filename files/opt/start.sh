121850ce725990e1b0ec1021c60bc5df69eaf893
Manuel Kiessling <manuel@kiessling.net>
2014-10-22 17:37:18 +0200

    Trying to get env parameter passing to work

--- files/opt/start.sh
+++ files/opt/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo ${COREOS_PRIVATE_IPV4} >> /etc/mysql/conf.d/wsrep_node_address.cnf
-
+echo $GALERA_WSREP_NODE_ADDRESS >> /etc/mysql/conf.d/wsrep_node_address.cnf
+while true; do echo "hello"; sleep 2; done
 service mysql start
 
 # Start confd watcher script
